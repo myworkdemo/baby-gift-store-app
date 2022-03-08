49094306244efb6c2c04488f4f6ed2b021afb18d
Ramkrishna Sitap <myworkdemo01@gmail.com>
2022-03-08 12:45:15 +0530

    baby-gift-store-api_spring-boot_code

---
+++ baby-gift-store-API/src/main/java/com/baby/gift/store/BabyGiftStoreApplication.java
@@ -0,0 +1,13 @@
+package com.baby.gift.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyGiftStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyGiftStoreApplication.class, args);
+	}
+
+}
---
+++ baby-gift-store-API/src/test/java/com/baby/gift/store/BabyGiftStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baby.gift.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyGiftStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
