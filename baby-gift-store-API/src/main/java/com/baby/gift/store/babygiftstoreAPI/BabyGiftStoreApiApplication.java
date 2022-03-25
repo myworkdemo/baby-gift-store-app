d4e09fd6edfa0c3ed85e660f3c5e275d9a1ae068
Ramkrishna Sitap <myworkdemo01@gmail.com>
2022-03-25 10:48:10 +0530

    spring boot api code update

---
+++ baby-gift-store-API/src/main/java/com/baby/gift/store/babygiftstoreAPI/BabyGiftStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.baby.gift.store.babygiftstoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyGiftStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyGiftStoreApiApplication.class, args);
+	}
+
+}
---
+++ baby-gift-store-API/src/test/java/com/baby/gift/store/babygiftstoreAPI/BabyGiftStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baby.gift.store.babygiftstoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyGiftStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
