00ac9f5d7b4d3fe8da00c757798427862c663aa8
Ramkrishna Sitap <myworkdemo01@gmail.com>
2022-03-26 15:03:20 +0530

    spring boot api updated code

---
+++ baby-gift-store-API/src/main/java/com/baby/gift/store/controller/ProductPurchaseController.java
@@ -0,0 +1,18 @@
+package com.baby.gift.store.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/product-purchase")
+public class ProductPurchaseController {
+	
+	@GetMapping(value = "/msg")
+	public String sayHello() {
+		return "Hello User!";
+	}
+
+}
